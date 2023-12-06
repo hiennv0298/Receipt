711213a6c0d07ef719cb609ea1a3576089ef3ad7
Hien Nguyen <hiennv0298@gmail.com>
2023-12-06 18:21:02 +0700

    Add project files.

---
+++ Receipt/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Receipt
+{
+    partial class frmReceipt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDeposit = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtOrderDate = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtOrderNumber = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFoodName = new System.Windows.Forms.TextBox();
+            this.nmuQuantity = new System.Windows.Forms.NumericUpDown();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lsvReceipt = new System.Windows.Forms.ListView();
+            this.STT = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FoodName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Quantity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Price = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Total = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmuQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Khách Hàng";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(89, 36);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(234, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtDeposit
+            // 
+            this.txtDeposit.Location = new System.Drawing.Point(89, 74);
+            this.txtDeposit.Name = "txtDeposit";
+            this.txtDeposit.Size = new System.Drawing.Size(234, 20);
+            this.txtDeposit.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tiền cọc";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtOrderDate);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtOrderNumber);
+            this.groupBox1.Controls.Add(this.txtDeposit);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Location = new System.Drawing.Point(48, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 193);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin hóa đơn";
+            // 
+            // dtOrderDate
+            // 
+            this.dtOrderDate.CustomFormat = "dd/MM/yyyy";
+            this.dtOrderDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtOrderDate.Location = new System.Drawing.Point(89, 147);
+            this.dtOrderDate.Name = "dtOrderDate";
+            this.dtOrderDate.Size = new System.Drawing.Size(234, 20);
+            this.dtOrderDate.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ngày";
+            // 
+            // txtOrderNumber
+            // 
+            this.txtOrderNumber.Location = new System.Drawing.Point(89, 110);
+            this.txtOrderNumber.Name = "txtOrderNumber";
+            this.txtOrderNumber.Size = new System.Drawing.Size(234, 20);
+            this.txtOrderNumber.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Mã hóa đơn";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Controls.Add(this.nmuQuantity);
+            this.groupBox2.Controls.Add(this.txtPrice);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtFoodName);
+            this.groupBox2.Location = new System.Drawing.Point(47, 211);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(344, 193);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Món ăn";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(90, 67);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(234, 20);
+            this.txtPrice.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 106);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Số lượng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tên Món";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Giá";
+            // 
+            // txtFoodName
+            // 
+            this.txtFoodName.Location = new System.Drawing.Point(90, 32);
+            this.txtFoodName.Name = "txtFoodName";
+            this.txtFoodName.Size = new System.Drawing.Size(234, 20);
+            this.txtFoodName.TabIndex = 1;
+            // 
+            // nmuQuantity
+            // 
+            this.nmuQuantity.Location = new System.Drawing.Point(90, 104);
+            this.nmuQuantity.Name = "nmuQuantity";
+            this.nmuQuantity.Size = new System.Drawing.Size(234, 20);
+            this.nmuQuantity.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(225, 139);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(99, 36);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Thêm món";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lsvReceipt
+            // 
+            this.lsvReceipt.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.STT,
+            this.FoodName,
+            this.Quantity,
+            this.Price,
+            this.Total});
+            this.lsvReceipt.GridLines = true;
+            this.lsvReceipt.HideSelection = false;
+            this.lsvReceipt.Location = new System.Drawing.Point(407, 12);
+            this.lsvReceipt.MultiSelect = false;
+            this.lsvReceipt.Name = "lsvReceipt";
+            this.lsvReceipt.Size = new System.Drawing.Size(706, 392);
+            this.lsvReceipt.TabIndex = 6;
+            this.lsvReceipt.UseCompatibleStateImageBehavior = false;
+            this.lsvReceipt.View = System.Windows.Forms.View.Details;
+            // 
+            // STT
+            // 
+            this.STT.Text = "STT";
+            this.STT.Width = 40;
+            // 
+            // FoodName
+            // 
+            this.FoodName.Text = "Tên Món";
+            this.FoodName.Width = 200;
+            // 
+            // Quantity
+            // 
+            this.Quantity.Text = "Số lượng";
+            // 
+            // Price
+            // 
+            this.Price.Text = "Đơn giá";
+            this.Price.Width = 150;
+            // 
+            // Total
+            // 
+            this.Total.Text = "Thành tiền";
+            this.Total.Width = 250;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(1014, 418);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(99, 36);
+            this.btnPrint.TabIndex = 6;
+            this.btnPrint.Text = "In Hóa Đơn";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // frmReceipt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 466);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.lsvReceipt);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmReceipt";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hóa Đơn";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmuQuantity)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDeposit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFoodName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtOrderDate;
+        private System.Windows.Forms.TextBox txtOrderNumber;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.NumericUpDown nmuQuantity;
+        private System.Windows.Forms.ListView lsvReceipt;
+        private System.Windows.Forms.ColumnHeader STT;
+        private System.Windows.Forms.ColumnHeader FoodName;
+        private System.Windows.Forms.ColumnHeader Quantity;
+        private System.Windows.Forms.ColumnHeader Price;
+        private System.Windows.Forms.ColumnHeader Total;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ Receipt/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+using static System.Windows.Forms.ListView;
+
+namespace Receipt
+{
+    public partial class frmReceipt : Form
+    {
+        public int NumberOrder { get; set; } = 1;
+        public frmReceipt()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtFoodName.Text) || string.IsNullOrWhiteSpace(txtPrice.Text) || !int.TryParse(txtPrice.Text, out _) || nmuQuantity.Value <= 0)
+            {
+                MessageBox.Show("Nhập đủ tên món, giá, số lượng", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var price = int.TryParse(txtPrice.Text, out int p) ? p : 0;
+            var total = int.TryParse(nmuQuantity.Text, out int q) ? q * price : 0;
+            string[] item = { NumberOrder.ToString(), txtFoodName.Text, nmuQuantity.Value.ToString(), price.ToString("N0"), total.ToString("N0") };
+            lsvReceipt.Items.Add(new ListViewItem(item));
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtOrderNumber.Text) || (!int.TryParse(txtDeposit.Text, out _) && !string.IsNullOrWhiteSpace(txtDeposit.Text)))
+            {
+                MessageBox.Show("Nhập đúng thông tin khách hàng", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Set the paper size for the receipt (80mm width)
+            PaperSize paperSize = new PaperSize("CustomReceipt", Convert.ToInt32(80 * 0.03937 * 100), 32767); // 0.03937 inch per mm
+            printDocument1.DefaultPageSettings.PaperSize = paperSize;
+            printDocument1.PrintPage += (s, d) => DrawListViewItems(d.Graphics, lsvReceipt.Items);
+            PrintPreviewDialog printPreviewDialog = new PrintPreviewDialog();
+            printPreviewDialog.Document = printDocument1;
+            printPreviewDialog.PrintPreviewControl.Zoom = 1.0;
+            DialogResult result = printPreviewDialog.ShowDialog();
+            // Start printing
+            // printDocument1.Print();
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font boldFont = new Font("Arial", 9, FontStyle.Bold);
+            Font boldLargeFont = new Font("Arial", 12, FontStyle.Bold);
+            Font boldSmallFont = new Font("Arial", 7, FontStyle.Bold);
+            Font regularFont = new Font("Arial", 8);
+            SolidBrush brush = new SolidBrush(Color.Black);
+
+            float lineHeight = 16;
+
+            StringFormat centerFormat = new StringFormat
+            {
+                Alignment = StringAlignment.Center
+            };
+
+            // Header content
+            string[] headerLines = {
+                                    "NHÀ HÀNG NAI VÀNG NEW",
+                                    "27 Nguyễn Văn Bá, P. Bình Thọ, TP. Thủ Đức",
+                                    "0907 106 391 - 0934 000 597",
+                                    "PHIẾU TẠM TÍNH"
+                                };
+
+            // Draw each line of the header
+            for (int i = 0; i < headerLines.Length; i++)
+            {
+                float y = i * lineHeight + 15;
+                e.Graphics.DrawString(headerLines[i], (i == 0 || i == 3) ? i == 0 ? boldFont : boldLargeFont : regularFont, brush, 157, i == 3 ? y + 10 : y, centerFormat);
+            }
+
+            e.Graphics.DrawString($"Số: {txtOrderNumber.Text}", boldSmallFont, brush, 157, 100, centerFormat);
+            e.Graphics.DrawString($"Ngày: ", boldFont, brush, 10, 129);
+            e.Graphics.DrawString(dtOrderDate.Text, regularFont, brush, 50, 130);
+            e.Graphics.DrawString($"Bàn: A33", boldLargeFont, brush, 8, 150);
+            e.Graphics.DrawString($"Thu ngân: ", boldFont, brush, 10, 174);
+            e.Graphics.DrawString($"Nguyễn Văn Hiền", regularFont, brush, 75, 175);
+
+
+            // If there is more content to print, set the HasMorePages property to true
+            // This will trigger another PrintPage event
+            e.HasMorePages = false;
+        }
+
+        void DrawListViewItems(Graphics graphics, ListViewItemCollection items)
+        {
+            // Set up the font and brush for drawing
+            Font font = new Font("Arial", 8);
+            SolidBrush brush = new SolidBrush(Color.Black);
+
+            // Set up column widths and spacing for 80mm paper width
+            float paperWidth = 80; // in mm
+            float totalColumnWidth = paperWidth - 20; // Subtracting padding
+            float spacing = 8;
+            float x = 10; // Initial x-coordinate
+            float y = 200; // Initial y-coordinate
+
+            // Calculate column widths based on the available space
+            float columnWidth = totalColumnWidth / items[0].SubItems.Count;
+
+            var pen = new Pen(Color.Black);
+            graphics.DrawLine(pen, 9, y - 2, 305, y - 2);
+            graphics.DrawString("#", font, brush, x + 5, y);
+            graphics.DrawLine(pen, x, y - 2, x, y + font.Height + 2);
+            graphics.DrawLine(pen, x + 20, y - 2, x + 20, y + font.Height + 2);
+            graphics.DrawString("Tên hàng", font, brush, x + 20, y);
+            graphics.DrawLine(pen, x + 135, y - 2, x + 135, y + font.Height + 2);
+            graphics.DrawString("SL", font, brush, x + 140, y);
+            graphics.DrawLine(pen, x + 165, y - 2, x + 165, y + font.Height + 2);
+            graphics.DrawString("Đơn giá", font, brush, x + 170, y);
+            graphics.DrawLine(pen, x + 225, y - 2, x + 225, y + font.Height + 2);
+            graphics.DrawString("TT", font, brush, x + 250, y);
+            graphics.DrawLine(pen, x + 295, y - 2, x + 295, y + font.Height + 2);
+            graphics.DrawLine(pen, 9, y + font.Height + 2, 305, y + font.Height + 2);
+
+
+            y += 20;
+
+            //// Draw data rows with gridlines
+            //foreach (ListViewItem item in lsvReceipt.Items)
+            //{
+            //    x = 10; // Reset x-coordinate for each row
+
+            //    for (int i = 0; i < item.SubItems.Count; i++)
+            //    {
+            //        // Draw vertical line for each column
+            //        graphics.DrawLine(pen, x, y, x, y + font.Height);
+
+            //        // Draw data text
+            //        graphics.DrawString(item.SubItems[i].Text, font, brush, x, y);
+
+            //        x += columnWidth + spacing;
+            //    }
+
+            //    // Draw horizontal line below the data row
+            //    graphics.DrawLine(pen, 10, y + font.Height, x, y + font.Height);
+
+            //    y += font.Height + spacing; // Move to the next row
+            //}
+        }
+    }
+}
